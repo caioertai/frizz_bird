3fd51da3ea9f82484a6659992cbb28c44be97b67
Caio Andrade <caioertai@gmail.com>
2018-04-24 01:01:51 -0300

    General linting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
+# app/controllers/pages_controller.rb
 class PagesController < ApplicationController
-  def home
-  end
+  def home; end
 end

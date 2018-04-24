c911728356caaa65e0a3b26ae79743ea60f18ff6
Caio Andrade <caioertai@gmail.com>
2018-04-24 04:29:11 -0300

    Created item visitor

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+# app/models/item.rb
 class Item < ApplicationRecord
   validates :path, presence: true
+
+  def visit
+    request = HTTParty.get("#{ENV['ext_source']}#{path}")
+    return unless request.code == 200
+    update(document: Nokogiri::HTML(request, nil, Encoding::UTF_8.to_s))
+  end
 end

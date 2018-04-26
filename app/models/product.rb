db0ba9365a210708694701b31a6b43d048ddeac8
Caio Andrade <caioertai@gmail.com>
2018-04-26 03:24:44 -0300

    Reverted mistake on product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,22 +15,6 @@ class Product < ApplicationRecord
     ParseService.new(self, attributes)
   end
 
-  def update_photo
-    self.remote_photo_url = ig_pic_url
-    save
-  rescue Cloudinary::CarrierWave::UploadError => exs
-    p exs.to_s
-    follower.visit
-    follower.promote!
-    reload
-    retry
-  rescue CloudinaryException => exs
-    p exs.to_s
-    p 'Waiting 2 seconds to retry'
-    sleep 2
-    retry
-  end
-
   (attribute_names + %w[ingredients]).each do |attribute|
     define_method :"parse_#{attribute}" do
       ParseService.new(self, [attribute.to_sym])

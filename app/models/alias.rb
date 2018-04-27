119569132e2b2eac7bada2fe4723a6ed910a3eb2
Caio Andrade <caioertai@gmail.com>
2018-04-27 14:56:54 -0300

    Ingredients and now case insensitive and alias auto changes its to match its ingredient with most products

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# app/models/alias.rb
 class Alias < ApplicationRecord
   has_many :ingredients
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,10 +2,25 @@
 
 # app/models/ingredient.rb
 class Ingredient < ApplicationRecord
-  validates :name, uniqueness: true, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 
-  belongs_to :alias
+  belongs_to :alias, optional: true
 
   has_many :product_ingredients, dependent: :destroy
   has_many :products, through: :product_ingredients
+
+  after_save :refresh_alias, if: :alias_id_changed?
+
+  def refresh_alias
+    # TODO: Make this a sql query
+    ordered_list = self.alias.ingredients.sort_by do |ingredient|
+      -ingredient.products.count
+    end
+    self.alias.update(name: ordered_list.first.name)
+  end
+
+  def self.find_or_create_by_name(name)
+    insensitive_match = Ingredient.where('lower(name) = ?', name.downcase).first
+    find_or_create_by(name: insensitive_match ? insensitive_match.name : name)
+  end
 end

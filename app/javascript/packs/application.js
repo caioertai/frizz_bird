7cc406a28dec61f43f5010e4361644e5151c2abe
Caio Andrade <caioertai@gmail.com>
2018-04-28 21:11:42 -0300

    Scroll after search and asset reordering

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 import { homeSkrollr } from '../components/home_skrollr';
-import { setScrollTo } from '../components/scroll_to';
+import { setScrollTo, documentScroll } from '../components/scroll_to';
 import "bootstrap";
 
 homeSkrollr();
-setScrollTo();
+documentScroll();

a73586ad7d8c9257c5e5250d9c97920f368ceb2c
Caio Andrade <caioertai@gmail.com>
2018-04-30 15:25:13 -0300

    Product show disables loading state

--- app/javascript/components/loading_feedback.js
+++ app/javascript/components/loading_feedback.js
@@ -2,9 +2,21 @@ function loadingFeedback() {
   const productSearch = document.getElementById('product-search');
   const landing = document.getElementById('landing');
 
+  // Adds loading function to document
+  const setMainLoading = (state) => {
+    if (state) {
+      productSearch.setAttribute('disabled', '');
+      landing.classList.add('loading');
+    } else {
+      productSearch.removeAttribute('disabled');
+      landing.classList.remove('loading');
+    }
+  };
+  document.setMainLoading = setMainLoading;
+
+  // Sets loading true on submit
   const submitTrigger = () => {
-    productSearch.setAttribute('disabled', '');
-    landing.classList.add('loading');
+    document.setMainLoading(true);
   };
   productSearch.addEventListener('submit', submitTrigger);
 }

b3c2f61f882c468868ce5ed5457a90bde2c7a339
Caio Andrade <caioertai@gmail.com>
2018-05-02 12:55:51 -0300

    Refactored loading feedback js

--- app/javascript/components/loading_feedback.js
+++ app/javascript/components/loading_feedback.js
@@ -1,25 +1,23 @@
 function loadingFeedback() {
-  const productSearchForm = document.getElementById('product-search');
-  const productTopbarSearchForm = document.getElementById('topbar-product-search');
-  const productSearchInput = document.getElementById('query');
-  const productTopbarSearchInput = document.getElementById('topbar-query');
+  const mainElements = document.querySelectorAll('.disable-on-loading');
+  const formElements = document.querySelectorAll('form.disable-on-loading');
   const body = document.getElementById('body');
 
   // Adds loading function to document
   const setMainLoading = (state) => {
     if (state) {
       body.classList.add('loading');
-      productSearchForm.setAttribute('disabled', '');
-      productTopbarSearchForm.setAttribute('disabled', '');
       // Timeout needed to delay form disabling until parameters are sent
-      setTimeout(() => { productSearchInput.setAttribute('disabled', ''); }, 0);
-      setTimeout(() => { productTopbarSearchInput.setAttribute('disabled', ''); }, 0);
+      setTimeout(() => {
+        mainElements.forEach((element) => {
+          element.setAttribute('disabled', '');
+        });
+      }, 0);
     } else {
       body.classList.remove('loading');
-      productSearchForm.removeAttribute('disabled');
-      productTopbarSearchForm.removeAttribute('disabled');
-      productSearchInput.removeAttribute('disabled');
-      productTopbarSearchInput.removeAttribute('disabled');
+      mainElements.forEach((element) => {
+        element.removeAttribute('disabled');
+      });
     }
   };
   document.setMainLoading = setMainLoading;
@@ -28,8 +26,9 @@ function loadingFeedback() {
   const submitTrigger = () => {
     document.setMainLoading(true);
   };
-  productSearchForm.addEventListener('submit', submitTrigger);
-  productTopbarSearchForm.addEventListener('submit', submitTrigger);
+  formElements.forEach((element) => {
+    element.addEventListener('submit', submitTrigger);
+  });
 }
 
 export default loadingFeedback;

568e81357eb48127927d252c7ff0c84312eab675
Caio Andrade <caioertai@gmail.com>
2018-05-02 01:59:25 -0300

    Main topbar functionalities

--- app/javascript/components/loading_feedback.js
+++ app/javascript/components/loading_feedback.js
@@ -1,19 +1,25 @@
 function loadingFeedback() {
   const productSearchForm = document.getElementById('product-search');
+  const productTopbarSearchForm = document.getElementById('topbar-product-search');
   const productSearchInput = document.getElementById('query');
-  const landing = document.getElementById('landing');
+  const productTopbarSearchInput = document.getElementById('topbar-query');
+  const body = document.getElementById('body');
 
   // Adds loading function to document
   const setMainLoading = (state) => {
     if (state) {
-      landing.classList.add('loading');
+      body.classList.add('loading');
       productSearchForm.setAttribute('disabled', '');
+      productTopbarSearchForm.setAttribute('disabled', '');
       // Timeout needed to delay form disabling until parameters are sent
       setTimeout(() => { productSearchInput.setAttribute('disabled', ''); }, 0);
+      setTimeout(() => { productTopbarSearchInput.setAttribute('disabled', ''); }, 0);
     } else {
-      landing.classList.remove('loading');
+      body.classList.remove('loading');
       productSearchForm.removeAttribute('disabled');
+      productTopbarSearchForm.removeAttribute('disabled');
       productSearchInput.removeAttribute('disabled');
+      productTopbarSearchInput.removeAttribute('disabled');
     }
   };
   document.setMainLoading = setMainLoading;
@@ -23,6 +29,7 @@ function loadingFeedback() {
     document.setMainLoading(true);
   };
   productSearchForm.addEventListener('submit', submitTrigger);
+  productTopbarSearchForm.addEventListener('submit', submitTrigger);
 }
 
 export default loadingFeedback;

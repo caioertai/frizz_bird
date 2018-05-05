385ab9f052dc6c66bb3694699fa5bc73f8a4c6e6
Caio Andrade <caioertai@gmail.com>
2018-05-05 15:20:15 -0300

    Done with mobile skrollr headache

--- app/javascript/components/home_skrollr.js
+++ app/javascript/components/home_skrollr.js
@@ -1,8 +1,11 @@
 import * as skrollr from 'skrollr';
 
 function homeSkrollr() {
-  document.skrollrInit = skrollr.init;
-  document.skrollrDestroy = skrollr.init().destroy;
+  const notMobile = !(/Android|iPhone|iPad|iPod|BlackBerry/i).test(navigator.userAgent || navigator.vendor || window.opera);
+  if (notMobile) {
+    document.skrollrInit = skrollr.init;
+    document.skrollrDestroy = skrollr.init().destroy;
+  }
 }
 
 export default homeSkrollr;

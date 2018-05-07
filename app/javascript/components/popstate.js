cf7f52803d6fbfb487ad11beb3e0761ddc4c32d9
Caio Andrade <caioertai@gmail.com>
2018-05-07 03:58:45 -0300

    Working popstate navigation. Missing case where product are search are different from previous

--- app/javascript/components/popstate.js
+++ app/javascript/components/popstate.js
@@ -1,9 +1,24 @@
+document.goTo = (actionName) => {
+  const actionElement = document.getElementById(`${actionName}`);
+  document.querySelectorAll('.content').forEach((element) => { element.classList.add('d-none'); });
+  actionElement.classList.remove('d-none');
+  document.smoothScrollTo(actionElement);
+};
+
 function popstate() {
   window.onpopstate = (event) => {
-    document.querySelectorAll('.content').forEach((element) => { element.classList.add('d-none'); });
-    const actionName = event.state.page;
-    const actionElement = document.getElementById(actionName);
-    actionElement.classList.remove('d-none');
+    const actionName = event.state && event.state.action;
+    if (actionName) {
+      // Checks content and gets if empty
+      const actionElement = document.getElementById(actionName);
+      if (actionElement.querySelector('.container').innerHTML === '') {
+        const fullPath = window.location.pathname + window.location.search;
+        document.setMainLoading(true);
+        $.get(fullPath);
+      } else {
+        document.goTo(actionName);
+      }
+    }
   };
 }
 
